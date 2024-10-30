b6f2044632e1d7abb1692641c90d751d6cba7f44
admin <codenamekirkpatrick@yandex.ru>
2024-10-31 00:48:13 +0300

    add templates

--- config/settings.py
+++ config/settings.py
@@ -119,21 +119,21 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-AUTH_USER_MODEL = "users.User"
+# AUTH_USER_MODEL = "users.User"
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 # LOGIN_URLS = '/users/'
-
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", False) == "True"
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", False) == "True"
-
-SERVER_EMAIL = EMAIL_HOST_USER
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+#
+# EMAIL_HOST = os.getenv("EMAIL_HOST")
+# EMAIL_PORT = os.getenv("EMAIL_PORT")
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+# EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", False) == "True"
+# EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", False) == "True"
+#
+# SERVER_EMAIL = EMAIL_HOST_USER
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 CACHE_ENABLED = True
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,79 @@
-from django.shortcuts import render
+from django.forms import inlineformset_factory
+from django.urls import reverse_lazy, reverse
 
-# Create your views here.
+from mailing.forms import MailingForm, MessageForm
+from mailing.models import Mailing, Message
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+class MailingListView(ListView):
+    model = Mailing
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list',)
+
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    form_class = MailingForm
+    success_url = reverse_lazy('mailing:mailing_list', )
+
+class MailingDeleteView(DeleteView):
+    model = Mailing
+    success_url = reverse_lazy('mailing:mailing_list')
+
+
+class MailingDetailView(DetailView):
+    model = Mailing
+
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    form_class = MessageForm
+    success_url = reverse_lazy('mailing:message_list',)
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    form_class = MessageForm
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        MessageFormset = inlineformset_factory(Message, Mailing, MailingForm, extra=1)
+        if self.request.method == "POST":
+            context_data['formset'] = MessageFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = MessageFormset()
+        return context_data
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+    def get_success_url(self):
+        return reverse('mailing:message_detail', args=[self.kwargs.get('pk')])
+
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    success_url = reverse_lazy('mailing:message_list')
+
+
+class MessageDetailView(DetailView):
+    model = Message
+
