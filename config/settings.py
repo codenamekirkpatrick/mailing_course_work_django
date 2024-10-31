4d53413800626a81c82ea1318ad0ab208dc0e9c9
admin <codenamekirkpatrick@yandex.ru>
2024-11-01 00:00:50 +0300

    add requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -106,9 +106,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_URL = "static/"
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (BASE_DIR / "static",)
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 
 # Default primary key field type
