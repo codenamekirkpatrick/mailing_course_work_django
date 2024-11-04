c5fb2e6b106064762dca8d497f0c52b2442f4220
admin <codenamekirkpatrick@yandex.ru>
2024-11-04 16:03:44 +0300

    create services2

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
             email="admin@admin.com",
-            first_name="Администратор",
-            last_name="Администраторов",
+            first_name="Admin",
+            last_name="Admin",
             is_staff=True,
             is_superuser=True,
             is_active=True,
